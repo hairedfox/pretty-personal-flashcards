b633d5da86cd3337b2ace74f48cf9e7046d18849
pvhiep96 <pvhiep96@gmail.com>
2021-03-28 15:58:14 +0700

    Sign up (#12)
    * Sign up
    
    * Fix comments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,21 @@ module ApplicationHelper
   def footer_copyright_year
     Time.current.year == 2021 ? 'present' : 2021
   end
+
+  def controller_stylesheet(options = { media: 'all' })
+    path = File.join(controller_path, action_name)
+    stylesheet_link_tag(path, options) if Rails.application.assets.find_asset(path)
+  end
+
+  def controller_javascript(options = {})
+    path = "#{controller_path}/#{action_name}"
+    javascript_pack_tag(path, options) if javascript_exists?(path)
+  end
+
+  private
+
+  def javascript_exists?(script)
+    script = "#{Rails.root}/app/javascript/#{script}.js"
+    File.exists?(script) || File.exists?("#{script}.coffee") 
+  end
 end
